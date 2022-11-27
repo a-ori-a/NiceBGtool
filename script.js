db5300f51bc93cb02f6d7a08669adb488a05b41c
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2022-11-27 17:45:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function accent(x, y) {
-    if (document.getElementById("accent").files.length == 1) {
-        var editor = document.getElementById("editor");
-        var ctx = editor.getContext("2d");
-        var reader = new FileReader();
-        reader.onload = function(event) {
-            var image = new Image();
-            image.onload = function() {
-                ctx.drawImage(image,x, y);
-            }
-            image.src = event.target.result;
-        }
-        reader.readAsDataURL(document.getElementById("accent").files[0])
-    }
-}
-
 function apply() {
     var editor = document.getElementById("editor");
     var ctx = editor.getContext("2d");
@@ -21,7 +5,6 @@ function apply() {
     var color = document.getElementById('color').value
     ctx.fillStyle = color
     ctx.fillRect(0,0,1920,1080)
-    placeImg()
 }
 
 function generate() {
@@ -31,7 +14,27 @@ function generate() {
     document.getElementById("newImg").src = png;
 }
 
-function placeImg() {
-    var position = document.getElementById('pos').value.split(',');
-    accent(position[0], position[1])
+function chRatio() {
+    var note = document.getElementById('viewValue');
+    var range = document.getElementById('ratio');
+    note.innerText = range.value;
+    putImg();
+}
+
+function putImg() {
+    apply();
+    if ( ! file.files[0])  {
+        return;
+    }
+    var editor = document.getElementById("editor");
+    var ctx = editor.getContext("2d");
+    var pos = document.getElementById('pos').value.split(',')
+    var ratio = document.getElementById('ratio').value
+    console.log(pos)
+    image = new Image()
+    image.onload = function() {
+        resize = {x: image.naturalWidth*ratio, y: image.naturalHeight*ratio}
+        ctx.drawImage(image, pos[0], pos[1], resize.x, resize.y);
+    }
+    image.src = URL.createObjectURL(file.files[0])
 }
